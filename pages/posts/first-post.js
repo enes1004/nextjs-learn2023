4432d1dc3be654de4bb4176962b9fdc0bc7e54b9
enes1004 <karabacakenes@gmail.com>
2023-03-04 00:11:55 +0900

    next basics completed

---
+++ pages/posts/first-post.js
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+import Script from 'next/script';
+import Layout from '../../components/layout';
+
+export default function FirstPost() {
+  return (
+     <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+       <h1>First Post</h1>
+       <h2>
+         <Link href="/">Back to home</Link>
+       </h2>
+       <Image
+         src="/images/profile.jpg" // Route of the image file
+         height={144} // Desired size with correct aspect ratio
+         width={144} // Desired size with correct aspect ratio
+         alt="Your Name"
+        />
+     </Layout>
+   );
+}
