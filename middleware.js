5f11036a22aa88bb78650fbda607f5b3c7cb8067
enes1004 <karabacakenes@gmail.com>
2023-03-13 20:51:32 +0900

    use withAuth instead

--- middleware.js
+++ middleware.js
@@ -1,9 +1,2 @@
-export { default } from 'next-auth/middleware';
-// import { withAuth } from 'next-auth/middleware';
-// export default withAuth({
-//   callbacks: {
-//     authorized: ({ token }) => {console.log(token);return !!token},
-//   },
-// })
-
-export const config = { secret:process.env.NEXTAUTH_SECRET }
+import { withAuth } from 'next-auth/middleware';
+export default withAuth({});
