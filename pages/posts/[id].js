21103c9ee18333dfbe684b6104bdce92951a4cc6
enes1004 <karabacakenes@gmail.com>
2023-04-08 04:08:12 +0900

    without auth in page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,10 +3,6 @@ import { getAllPostIds, getPostData,getPostDataForCaching,getAllPostIdsForCachin
 import Head from 'next/head';
 import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css';
-import { useContentGroupAuth,useContentAuthorAuth,authByApi } from "../../auth/authorize"
-import { authOptions } from '../api/auth/[...nextauth]'
-import { useSession } from "next-auth/react"
-import { useState,useEffect } from "react";
 
 // export async function getServerSideProps(info){
 //   console.log(info);
@@ -14,28 +10,10 @@ import { useState,useEffect } from "react";
 // }
 
 export default function Post({ postData }) {
-  const session=useSession();
-  const [hideContent,setHideContent]=useState(false);
-
-  const can_see_by_content_group=useContentGroupAuth(postData?.content_group_id,session?.data?.user);
-  const can_see_by_author=useContentAuthorAuth(postData?.user_id,session?.data?.user);
-  useEffect(()=>{
-    if(session && session.status!="loading"){
-      if(!can_see_by_author && !can_see_by_content_group){
-        setHideContent(true);
-      }
-      console.log("api_auth",authByApi(postData.id,session));
-    }
-  },[session]);
-
   return <Layout>
       <Head>
         <title>{postData?.title + " / " + postData?.slug}</title>
       </Head>
-      {hideContent?
-        <div className={utilStyles.lightText}>Not authorized</div>
-        :
-      <>
       <h1 className={utilStyles.headingXl}>{postData?.title}</h1>
       <div className={utilStyles.lightText}>
         By {(postData?.author?.name)??"Anonymous"}
@@ -44,8 +22,6 @@ export default function Post({ postData }) {
         {postData?<Date dateString={postData.created_at} />:null}
       </div>
       <div dangerouslySetInnerHTML={{ __html: postData?.content }} />
-      </>
-      }
       <div>{JSON.stringify(session.data)}</div>
   </Layout>;
 }
